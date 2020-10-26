8411c141af962f9ef955c2b997bd59790afc7c5b
tgpaul <68840617+tgpaul@users.noreply.github.com>
2020-10-26 18:31:08 +0530

    Add files via upload

--- Lab1_5.java
+++ Lab1_5.java
@@ -9,8 +9,11 @@ public class Lab1_5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int sum = 0;
 		for(int i=1;i<=n;i++) {
-			
+			sum += i;
 		}
+		System.out.println("The sum is "+sum);
+		sc.close();
 	}
 }
